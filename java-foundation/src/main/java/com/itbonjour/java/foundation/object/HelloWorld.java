1c0f18bae4c43753d2439c4f7de87eee4c878ec9
itbonjour <m598956554@163.com>
2021-07-17 19:41:51 +0800

    我的第一个Java程序

---
+++ java-foundation/src/main/java/com/itbonjour/java/foundation/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.itbonjour.java.foundation.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author itbonjour m598956554@qq.com
+ * @version 2021/7/17 7:40 下午
+ * @since JDK11
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("我的第一个Java程序");
+    }
+}
